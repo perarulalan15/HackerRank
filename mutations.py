c516161148fa6a084a7f1ceee8e6343d6bc7b9cc
Perarulalan V <95120766+PERARULALAN2004@users.noreply.github.com>
2022-12-16 19:14:04 +0530

    Add files via upload

---
+++ mutations.py
@@ -0,0 +1,8 @@
+def mutate_string(string, position, character):
+   s_new = string[:position] + character + string[position+1:]
+   return s_new
+if __name__ == '__main__':
+    s = input("Enter string:")
+    i, c = input("Enter position and character:").split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
