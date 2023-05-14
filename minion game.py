f3408044cc45e87b5fa5a83eab6bdf38758925e3
Perarulalan V <95120766+perarulalan15@users.noreply.github.com>
2023-05-14 09:02:53 +0530

    Add files via upload

---
+++ minion game.py
@@ -0,0 +1,17 @@
+def minion_game(string):
+    string=string.upper()
+    kevin=stuart=0
+    l=len(string)
+    for i, char in enumerate(string):
+        p=l-i
+        if char in {"A","E","I","O","U"}:
+            kevin += p
+        else:
+            stuart += p
+    if kevin == stuart:
+        print("Draw")
+    else:
+        print(*("Stuart",stuart) if stuart>kevin else ("Kevin",kevin))
+if __name__ == '__main__':
+    s = input("Enter string:")
+    minion_game(s)
