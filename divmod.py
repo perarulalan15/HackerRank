3810b72cff4f79194a155da58fe818ae013318a8
Perarulalan V <95120766+perarulalan15@users.noreply.github.com>
2023-05-29 10:46:07 +0530

    Add files via upload

---
+++ divmod.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+m=a//b
+n=a%b
+print(m)
+print(n)
+print(divmod(a,b))
---
+++ pow mod power.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+m=int(input("Enter m:"))
+print(pow(a,b))
+print(pow(a, b, m))
